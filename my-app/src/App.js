0d0b8467b7859504f5abd06c54b0ae77aa2040f7
Aman Chaturvedi <73428540+AmanChaturvedi24@users.noreply.github.com>
2022-10-29 22:40:38 +0530

    added toggle switch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,19 @@
-// import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 import Textbox from './components/Textbox';
 import Navbar from './components/Navbar';
-// import Textbox from './components/Textbox';
 function App() {
+  const [mood, setmood] = useState("light");
+  const toggleHandle=()=>{
+    if(mood==="light")
+    setmood("dark")
+    else
+    setmood("light")
+  }
   return (
     <>
-   <Navbar title='TexT_Man'/>
+   <Navbar title='TexT_Man' mode={mood} toggleHandle={toggleHandle}/>
    <div className="container my-3">
     <Textbox heading="Enter the text to analyze below"/>
     {/* <About/> */}
